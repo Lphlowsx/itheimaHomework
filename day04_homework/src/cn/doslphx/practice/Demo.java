70168fb25b286250faa2dcd3c89922b41f1051e0
doslphx <doslphx@gmail.com>
2020-07-24 18:06:22 +0800

    'test'

--- day04_homework/src/cn/doslphx/practice/Demo.java
+++ day04_homework/src/cn/doslphx/practice/Demo.java
@@ -1,7 +1,11 @@
 package cn.doslphx.practice;
+/*
+ *  @author:  CodingYoo
+ *  date:     7/24/2020  11:17 AM Fri
+ *  project:  itheimaHomework
+ *  github:   https://github.com/CodingYoo
+ *  from  那时还是个菜鸟
+ */
 
 public class Demo {
-    public static void main(String[] args) {
-
-    }
 }
