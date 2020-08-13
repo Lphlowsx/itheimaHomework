cc4d7c390fcd94c178865f48543111c674fa6302
doslphx <doslphx@gmail.com>
2020-08-13 21:23:35 +0800

    Test

---
+++ Java-base/src/cn/CodingYoo/Collection/Test.java
@@ -0,0 +1,14 @@
+package cn.CodingYoo.Collection;
+/*
+ *  @author:  CodingYoo
+ *  date:     8/13/2020  9:22 PM Thu
+ *  project:  itheimaHomework
+ *  github:   https://github.com/CodingYoo
+ *  from  那时还是个菜鸟
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("成功推送至GitHub和gitee");
+    }
+}
