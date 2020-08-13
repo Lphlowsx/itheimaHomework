f40e3b4d6c5365cc96b61b563d16fda51535f377
doslphx <doslphx@gmail.com>
2020-08-13 21:34:24 +0800

    Test02

---
+++ Java-base/src/cn/CodingYoo/Collection/Test02.java
@@ -0,0 +1,14 @@
+package cn.CodingYoo.Collection;
+/*
+ *  @author:  CodingYoo
+ *  date:     8/13/2020  9:33 PM Thu
+ *  project:  itheimaHomework
+ *  github:   https://github.com/CodingYoo
+ *  from  那时还是个菜鸟
+ */
+
+public class Test02 {
+    public static void main(String[] args) {
+        System.out.println("成功！");
+    }
+}
