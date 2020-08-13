cac87c29a8c55a15d4d5a01b7476cb843718e7de
doslphx <doslphx@gmail.com>
2020-08-13 16:26:48 +0800

    Collection使用练习

---
+++ Java-base/src/cn/CodingYoo/Collection/CollectionDemo.java
@@ -0,0 +1,33 @@
+package cn.CodingYoo.Collection;
+/*
+ *  @author:  CodingYoo
+ *  date:     8/13/2020  4:20 PM Thu
+ *  project:  itheimaHomework
+ *  github:   https://github.com/CodingYoo
+ *  from  那时还是个菜鸟
+ */
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class CollectionDemo {
+    public static void main(String[] args) {
+        Collection<Student> list = new ArrayList<>();
+        //创建学生对象
+        Student stu01 = new Student("吕朋辉","22","男");
+        Student stu02 = new Student("王山心","22","女");
+        Student stu03 = new Student("吕辉","22","男");
+        //将学生对象存入ArrayList集合中
+        list.add(stu01);
+        list.add(stu02);
+        list.add(stu03);
+
+        //使用迭代器遍历集合元素
+        Iterator<Student> it = list.iterator();
+        while (it.hasNext()){
+            Student student = it.next();
+            System.out.println(student);
+        }
+    }
+}
---
+++ Java-base/src/cn/CodingYoo/Collection/Student.java
@@ -0,0 +1,56 @@
+package cn.CodingYoo.Collection;
+/*
+ *  @author:  CodingYoo
+ *  date:     8/13/2020  4:18 PM Thu
+ *  project:  itheimaHomework
+ *  github:   https://github.com/CodingYoo
+ *  from  那时还是个菜鸟
+ */
+
+public class Student {
+    private String name;
+    private String age;
+    private String sex;
+
+    public Student() {
+    }
+
+    public Student(String name, String age, String sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
+}
