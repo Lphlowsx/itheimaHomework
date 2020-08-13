b0de91dfee2ff1c2198be11fba2d8a6f6c2db953
doslphx <doslphx@gmail.com>
2020-08-13 16:01:19 +0800

    迭代器删除指定元素

---
+++ Java-base/src/cn/CodingYoo/Collection/IteratorTest2.java
@@ -0,0 +1,43 @@
+package cn.CodingYoo.Collection;
+/*
+ *  @author:  CodingYoo
+ *  date:     8/13/2020  3:53 PM Thu
+ *  project:  itheimaHomework
+ *  github:   https://github.com/CodingYoo
+ *  from  那时还是个菜鸟
+ */
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class IteratorTest2 {
+    public static void main(String[] args) {
+        //迭代器删除指定元素
+        ArrayList<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+
+       /* //原始方法删除元素
+        for (int i = 0; i < list.size(); i++) {
+            String s = list.get(i);
+            if(s.equals("b")){
+                list.remove(i);
+                i--;
+            }
+        }
+        System.out.println(list);*/
+
+        //使用迭代器删除指定元素
+        Iterator<String> it = list.iterator();
+        while (it.hasNext()){
+            String s = it.next();
+            if("b".equals(s)){
+                it.remove();
+            }
+        }
+        System.out.println(list);
+    }
+}
