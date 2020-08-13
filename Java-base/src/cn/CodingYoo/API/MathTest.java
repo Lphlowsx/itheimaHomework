0be6a0007f0d92a122dd1100b14eb5446c76e14c
doslphx <doslphx@gmail.com>
2020-08-13 15:29:01 +0800

    使用迭代器遍历集合中的元素

---
+++ Java-base/src/cn/CodingYoo/API/MathTest.java
@@ -0,0 +1,17 @@
+package cn.CodingYoo.API;
+/*
+ *  @author:  CodingYoo
+ *  date:     8/11/2020  8:39 PM Tue
+ *  project:  itheimaHomework
+ *  github:   https://github.com/CodingYoo
+ *  from  那时还是个菜鸟
+ */
+
+public class MathTest {
+    public static void main(String[] args) {
+
+        //常用API
+        System.out.println(Math.max(12,11));
+
+    }
+}
---
+++ Java-base/src/cn/CodingYoo/Collection/IteratorTest.java
@@ -0,0 +1,28 @@
+package cn.CodingYoo.Collection;
+/*
+ *  @author:  CodingYoo
+ *  date:     8/13/2020  3:21 PM Thu
+ *  project:  itheimaHomework
+ *  github:   https://github.com/CodingYoo
+ *  from  那时还是个菜鸟
+ */
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class IteratorTest {
+    public static void main(String[] args) {
+        Collection<String> list = new ArrayList<>();
+        list.add("吕朋辉");
+        list.add("王山心");
+        list.add("张三");
+        list.add("李四");
+        Iterator<String> it = list.iterator();
+        //使用迭代器遍历集合中的每一个元素   hasNext() 判断集合中是否有元素  Next()默认指向零索引
+
+        while (it.hasNext()){
+            System.out.println(it.next());
+        }
+    }
+}
