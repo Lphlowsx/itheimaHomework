7f85a516dd7d54762f83b021a527cddf0da29d1c
doslphx <doslphx@gmail.com>
2020-08-28 14:58:15 +0800

    ArrayList_01 练习

---
+++ Java-base/src/cn/CodingYoo/Collection/List/ArrayList_01.java
@@ -0,0 +1,23 @@
+package cn.CodingYoo.Collection.List;
+/*
+ *  @author:  CodingYoo
+ *  date:     8/28/2020  2:19 PM Fri
+ *  project:  itheimaHomework
+ *  github:   https://github.com/CodingYoo
+ *  gitee:    https://gitee.com/CodingYoo
+ *  from  那时还是个菜鸟
+ */
+
+import java.util.ArrayList;
+
+public class ArrayList_01 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        for (Integer integer : list) {
+            System.out.println(integer);
+        }
+    }
+}
