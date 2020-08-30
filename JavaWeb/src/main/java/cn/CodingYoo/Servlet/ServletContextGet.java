e114864b5dfc107d2451ecea94ce392c5d8b72d6
doslphx <doslphx@gmail.com>
2020-08-30 15:05:19 +0800

    ServletContext应用

---
+++ JavaWeb/src/main/java/cn/CodingYoo/Servlet/ServletContextGet.java
@@ -0,0 +1,29 @@
+package cn.CodingYoo.Servlet;
+/*
+ *  @author:  CodingYoo
+ *  date:     8/30/2020  2:37 PM Sun
+ *  project:  itheimaHomework
+ *  github:   https://github.com/CodingYoo
+ *  gitee:    https://gitee.com/CodingYoo
+ *  from  那时还是个菜鸟
+ */
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ServletContextGet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //创建ServletContext对象
+        ServletContext servletContext = this.getServletContext();
+        String s = (String) servletContext.getAttribute("username");
+        //设置编码
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("utf-8");
+        resp.getWriter().println("姓名为："+ s);
+    }
+}
---
+++ JavaWeb/src/main/java/cn/CodingYoo/Servlet/ServletContextSet.java
@@ -0,0 +1,24 @@
+package cn.CodingYoo.Servlet;
+/*
+ *  @author:  CodingYoo
+ *  date:     8/30/2020  2:22 PM Sun
+ *  project:  itheimaHomework
+ *  github:   https://github.com/CodingYoo
+ *  gitee:    https://gitee.com/CodingYoo
+ *  from  那时还是个菜鸟
+ */
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ServletContextSet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletContext servletContext = this.getServletContext();
+        String s = "吕朋辉";
+        servletContext.setAttribute("username",s);
+    }
+}
