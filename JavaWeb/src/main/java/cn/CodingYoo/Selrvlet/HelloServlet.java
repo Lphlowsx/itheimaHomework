f1d9df28c1388c1daf8299d5d63073d0c66e42c8
doslphx <doslphx@gmail.com>
2020-08-30 11:06:10 +0800

    Hello Servlet！

---
+++ JavaWeb/src/main/java/cn/CodingYoo/Selrvlet/HelloServlet.java
@@ -0,0 +1,32 @@
+package cn.CodingYoo.Selrvlet;
+/*
+ *  @author:  CodingYoo
+ *  date:     8/30/2020  10:35 AM Sun
+ *  project:  itheimaHomework
+ *  github:   https://github.com/CodingYoo
+ *  gitee:    https://gitee.com/CodingYoo
+ *  from  那时还是个菜鸟
+ */
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //ServletOutputStream outputStream = resp.getOutputStream(); 获得输出流
+        PrintWriter writer = resp.getWriter();
+        writer.println("hello Servlet!");
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
