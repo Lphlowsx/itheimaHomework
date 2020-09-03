88b6a2f81623ebbfc7476514e07841b9884b66a7
doslphx <doslphx@gmail.com>
2020-09-03 14:42:42 +0800

    response  使用

---
+++ JavaWeb/src/main/java/cn/CodingYoo/Servlet/ServletRes.java
@@ -0,0 +1,30 @@
+package cn.CodingYoo.Servlet;
+/*
+ *  @author:  CodingYoo
+ *  date:     9/3/2020  2:27 PM Thu
+ *  github:   https://github.com/CodingYoo
+ *  gitee:    https://gitee.com/CodingYoo
+ */
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/ServletRes")
+public class ServletRes extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        System.out.println(username);
+        System.out.println(password);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
--- JavaWeb/src/main/webapp/index.jsp
+++ JavaWeb/src/main/webapp/index.jsp
@@ -11,7 +11,7 @@
     <title>jspTest</title>
 </head>
 <body>
-<form action="${pageContext.request.contextPath}/Redirect" method="get">
+<form action="${pageContext.request.contextPath}/ServletRes" method="get">
     用户名：<input type="text" name="username"> <br>
     密码：<input type="password" name="password"><br>
     <input type="submit" >
