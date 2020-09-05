4d226960a77d51513749079a42b1569f493055a6
doslphx <doslphx@gmail.com>
2020-09-05 10:28:21 +0800

    Filter过滤字符，防止乱码

--- JavaWeb/src/main/java/cn/CodingYoo/Filter/FilterTest.java
+++ JavaWeb/src/main/java/cn/CodingYoo/Filter/FilterTest.java
@@ -16,6 +16,10 @@ public class FilterTest implements Filter {
     }
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        resp.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html;charset=utf-8");
+        System.out.println("过滤器起作用了");
         chain.doFilter(req, resp);
     }
 
